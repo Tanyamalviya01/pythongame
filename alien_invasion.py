365e8301fd0d90fda1935679b20b916d6e3eb3f7
Tanyamalviya01 <tanya_malviya1@baylor.edu>
2025-06-26 16:50:27 +0530

    Final revisions and project completion

--- alien_invasion.py
+++ alien_invasion.py
@@ -14,6 +14,7 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+        self.settings = Settings()  
         self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
