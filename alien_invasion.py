66a48fca1025dce728a8db88226d57ca4a1c4603
Tanya Malviya <tanya_malviya1@baylor.edu>
2025-06-26 08:56:27 +0000

    Added ship hit message when ship is hit

--- alien_invasion.py
+++ alien_invasion.py
@@ -86,6 +86,9 @@ class AlienInvasion:
         """Update the positions of all aliens in the fleet."""
         self._check_fleet_edges()
         self.aliens.update()
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+         print("Ship hit!!!")
 
 
     
